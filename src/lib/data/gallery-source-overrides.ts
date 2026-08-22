ca7a7831f0fd0d5b038e2d5345a830adab413cff
mrpersonalbot <mrpersonalbot@gmail.com>
2026-08-22 10:06:21 +0700

    pin clean exact galleries for weak products

--- src/lib/data/gallery-source-overrides.ts
+++ src/lib/data/gallery-source-overrides.ts
@@ -2,19 +2,47 @@ export const gallerySourceOverrides: Record<string, string> = {
   "nike-alphafly-3": "https://www.nike.com/id/t/alphafly-3-road-racing-shoes-d6x9mh/FD8311-101",
   "nike-vaporfly-4": "https://www.nike.com/id/t/vaporfly-4-road-racing-shoes-PTwDtp/HF6414-112",
   "nike-streakfly-2": "https://www.nike.ae/en/streakfly-2-proto-mens-road-racing-shoes/NKHF6417-100.html",
+
+  "adidas-adizero-adios-pro-4": "https://www.adidas.com/us/adizero-adios-pro-4-shoes/JQ9702.html",
+  "adidas-prime-x3-strung": "https://www.adidas.co.id/en/adizero-prime-x3-strung-shoes/JR2597.html",
+  "adidas-terrex-agravic-speed-ultra": "https://www.adidas.com/us/terrex-agravic-speed-ultra-trail-running-shoes/IH3764.html",
+  "adidas-supernova-rise-3": "https://www.adidas.co.id/en/supernova-rise-3-running-shoes/JR2280.html",
+  "adidas-supernova-prima-2": "https://www.adidas.co.id/id/sepatu-supernova-prima-2/JR3195.html",
+  "adidas-ultraboost-5": "https://www.adidas.co.id/en/ultraboost-5-shoes/JH9072.html",
+  "adidas-hyperboost-edge": "https://www.adidas.com/us/hyperboost-edge-running-shoes/KK0290.html",
+
   "hoka-speedgoat-6": "https://au.hoka.com/products/m-speedgoat-6-1147791-bblc-bblc",
-  "puma-velocity-nitro-5": "https://id.puma.com/en/pd/velocity-nitro%E2%84%A2-5-running-shoes-men/312944.html?dwvar_312944_color=05&dwvar_312944_size=0220",
-  "on-cloudflow-5": "https://www.on.com/en-jp/products/cloudflow-5-m-3mf1011/mens/arctic-stone-shoes-3MF10113306",
-  "brooks-glycerin-22": "https://www.fleetfeet.com/products/mens-brooks-glycerin-22?sku=110445-1D-090",
-  "saucony-kinvara-16": "https://www.holabirdsports.com/products/saucony-kinvara-16-mens-black-white",
-  "saucony-ride-19": "https://www.saucony.com/RO/en_RO/ride-19/60823M.html?dwvar_60823M_color=S21055-172",
-  "skechers-aero-razor": "https://www.skechers.id/en/products/skechers-skx-aero-razor-mens-running-shoes-white",
-  "skechers-aero-burst": "https://www.skechers.com/skechers-slip-ins-aero-burst/246215_WSL.html",
+  "hoka-rocket-x-3": "https://au.hoka.com/products/u-rocket-x-3-1168724-bkgd-bkgd",
   "hoka-cielo-x1-2": "https://www.fleetfeet.com/products/hoka-cielo-x1-2-0?sku=1162053-FLCK&width=D",
   "hoka-skyward-x": "https://runpacers.com/products/mens-hoka-one-one-skyward-x-blanc-de-blanc-virtual-blue",
   "hoka-mafate-x": "https://www.fleetfeet.com/products/mens-hoka-mafate-x?sku=1161990-BCMN",
   "hoka-challenger-8": "https://www.fleetfeet.com/products/mens-hoka-challenger-8?sku=1168716-JDT",
+
+  "new-balance-sc-trainer-v3": "https://www.newbalance.com/pd/fuelcell-supercomp-trainer-v3/MRCX25R-D-11.html",
+
+  "puma-velocity-nitro-5": "https://id.puma.com/en/pd/velocity-nitro%E2%84%A2-5-running-shoes-men/312944.html?dwvar_312944_color=05&dwvar_312944_size=0220",
+  "puma-fast-r-nitro-elite-3": "https://id.puma.com/en/pd/fast-r-nitro%E2%84%A2-elite-3-running-shoes-men/312060.html?dwvar_312060_color=02",
+  "puma-magnify-nitro-3": "https://us.puma.com/us/en/pd/magnify-nitro-3-running-shoes-men/311046?size=0220&swatch=01",
+
+  "brooks-glycerin-22": "https://www.fleetfeet.com/products/mens-brooks-glycerin-22?sku=110445-1D-090",
+  "brooks-hyperion-elite-5": "https://www.brooksrunning.com/en_us/featured/unisex-running-shoes/hyperion-elite-5/1000491D756.050.html",
+  "brooks-cascadia-19": "https://www.brooksrunning.com/en_us/mens/shoes/trail-shoes/cascadia-19/110457.html?dwvar_110457_color=098",
+
+  "saucony-kinvara-16": "https://www.holabirdsports.com/products/saucony-kinvara-16-mens-black-white",
+  "saucony-ride-19": "https://www.run4it.com/products/saucony-mens-ride-19-running-shoes-white-crimson",
+  "saucony-endorphin-elite-2": "https://www.saucony.com/en/endorphin-elite-2/59824U.html?dwvar_59824U_color=S30994-172",
+  "saucony-hurricane-25": "https://www.saucony.com/en/hurricane-25/60323M.html?dwvar_60323M_color=S21026-106",
+
+  "on-cloudflow-5": "https://www.on.com/en-jp/products/cloudflow-5-m-3mf1011/mens/arctic-stone-shoes-3MF10113306",
+  "on-cloudultra-pro": "https://www.on.com/en-us/products/cloudultra-pro-m-3mf3017/mens/pearl-lychee-shoes-3MF30174107",
+
+  "mizuno-wave-sky-9": "https://jpn.mizuno.com/ec/disp/attgrp/J1GC2502/",
+
   "asics-gt-2000-14": "https://www.asics.com.tr/en/gt-2000-14-756",
+  "asics-gel-kayano-33": "https://www.asics.com/sg/en-sg/gel-kayano-33/p/1011C167-001.html",
+
+  "skechers-aero-razor": "https://www.skechers.id/en/products/skechers-skx-aero-razor-mens-running-shoes-white",
+  "skechers-aero-burst": "https://www.skechers.com/skechers-slip-ins-aero-burst/246215_WSL.html",
 
   "ortuseight-hyperglide-3-1": "https://www.projeksportsampang.com/products/ortuseight-hyperglide-3-1-white-cyan",
   "ortuseight-hyperblast-3": "https://startinglane.co.id/product/11040200",
