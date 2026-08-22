541d396022e32b98218f194dace81bf2e078b549
mrpersonalbot <mrpersonalbot@gmail.com>
2026-08-22 11:03:45 +0700

    replace remaining weak gallery sources

--- src/lib/data/gallery-fixes.ts
+++ src/lib/data/gallery-fixes.ts
@@ -12,8 +12,11 @@ export const galleryFixes: Record<string, ShoeImageView[]> = {
   ],
   "nike-streakfly-2": [
     { label: "Side", url: "https://www.nike.qa/dw/image/v2/BDVB_PRD/on/demandware.static/-/Sites-akeneo-master-catalog/default/dwcb3c4d90/nk/3d6/3/5/0/7/5/3d635075_e355_4037_8583_b934cee6087b.png?q=100&sh=700&sm=fit&strip=false&sw=700" },
-    { label: "Top", url: "https://img.alpen-group.jp/Contents/ProductSubImages/0/4301563416-0001_sub03_LL.jpg" },
+    { label: "Top", url: "https://www.nike.ae/dw/image/v2/BDVB_PRD/on/demandware.static/-/Sites-akeneo-master-catalog/default/dwb156b185/nk/7b0/3/1/0/1/1/7b031011_bc1c_4602_9ada_6441a6f67031.png?sh=2000&sm=fit&sw=2000" },
     { label: "Outsole", url: "https://media.rundna.com.au/9efe9df0-68dd-468e-86b7-c9fdfe41a909.jpg" },
+  ],
+  "nike-vaporfly-4": [
+    { label: "Side", url: "https://0990b9.a-cdn.akinoncloud.com/products/2025/08/13/7607922/bf0421c2-b1a4-4314-a0eb-57b54f1402e1_size3840x3840_cropCenter.jpg" },
   ],
   "hoka-speedgoat-6": [
     { label: "Side", url: "https://cdn11.bigcommerce.com/s-eeul26hjka/images/stencil/1280x1280/products/4330/137524/media__49340.1736331696.jpg?c=1&imbypass=on" },
@@ -30,10 +33,40 @@ export const galleryFixes: Record<string, ShoeImageView[]> = {
     { label: "Alternate", url: "https://nb.scene7.com/is/image/NB/mrcellr5_nb_03_i?fmt=webp&wid=1200" },
     { label: "Alternate", url: "https://nb.scene7.com/is/image/NB/mrcellr5_nb_04_i?fmt=webp&wid=1200" },
   ],
+  "puma-deviate-pure-nitro": [
+    { label: "Side", url: "https://media.rundna.com.au/bf655262-ce2a-4d68-9f81-e0e2c74b9e3d__L.jpg" },
+    { label: "Top", url: "https://cdn.shopify.com/s/files/1/0603/3031/1875/files/7_8727761d-32ad-465f-807d-7f7f3272ccc0_1080x.jpg?=75&v=1780626407" },
+  ],
   "saucony-endorphin-pro-5": [
     { label: "Side", url: "https://www.paceathletic.com/cdn/shop/files/Mens-Saucony-Endorphin-Pro-5-White_Black-S21064-101.jpg?v=1769557991&width=1701" },
     { label: "Top", url: "https://www.paceathletic.com/cdn/shop/files/Mens-Saucony-Endorphin-Pro-5-White_Black-S21064-101-2.jpg?v=1769558018&width=1701" },
     { label: "Outsole", url: "https://www.paceathletic.com/cdn/shop/files/Mens-Saucony-Endorphin-Pro-5-White_Black-S21064-101-5.jpg?v=1769558077&width=1701" },
+  ],
+  "saucony-kinvara-16": [
+    { label: "Side", url: "https://cdn.blazimg.com/1800/product/s/a/saucony_s21020-200_2-nw091825.webp" },
+  ],
+  "brooks-glycerin-22": [
+    { label: "Side", url: "https://runninglabstore.com/cdn/shop/files/110445_020_L_Glycerin_22_1200x1200.png?v=1737410573" },
+  ],
+  "on-cloudsurfer-2": [
+    { label: "Side", url: "https://media.sivasdescalzo.com/media/catalog/product/3/M/3MF10123334_sivasdescalzo-On-CLOUDSURFER_2-1741965831-1.jpg" },
+    { label: "Top", url: "https://noirfonce.eu/cdn/shop/files/On_Cloudsurfer_2_Ivory_Ivory_3MF10123334_4.jpg?crop=center&height=1500&v=1739915198&width=1000" },
+    { label: "Outsole", url: "https://www.sneakinpeace.com/cdn/shop/files/on-running-cloudsurfer-2-ivory-ivory-running-shoes-3_16e781ac-a189-49e7-8619-fe6d50d50f13_1600x2000.jpg?v=1739693308" },
+  ],
+  "on-cloudflow-5": [
+    { label: "Side", url: "https://imagedelivery.net/2DfovxNet9Syc-4xYpcsGg/88c263cd-46d2-468d-9f8a-884cfaa07800/public" },
+    { label: "Top", url: "https://bananarun.com/media/catalog/product/cache/aa5b17117e43c0b36f21fad92985fb3e/3/m/3mf10113306-cloudflow_5-fw25-arctic_stone-m-1x1-g2.png" },
+    { label: "Outsole", url: "https://images.prodirectsport.com/productimages/Gallery_3/1021860_Gallery_3_2016845.jpg" },
+  ],
+  "skechers-aero-razor": [
+    { label: "Side", url: "https://cdn.shopify.com/s/files/1/0603/3031/1875/files/246240_WBO_HERO_LG_converted_3840x.jpg?=75&v=1771836404" },
+    { label: "Top", url: "https://cdn.shopify.com/s/files/1/0603/3031/1875/files/246240_WBO_INSOLE_converted_3840x.jpg?=75&v=1771836404" },
+    { label: "Outsole", url: "https://cdn.shopify.com/s/files/1/0603/3031/1875/files/246240_WBO_OUTSOLE_converted_3840x.jpg?=75&v=1771836404" },
+  ],
+  "skechers-aero-burst": [
+    { label: "Side", url: "https://cdn.idealo.com/folder/Product/207854/0/207854090/s2_produktbild_max_2/skechers-aero-burst-246210-black-white.jpg" },
+    { label: "Top", url: "https://cdn.idealo.com/folder/Product/207854/0/207854090/s11_produktbild_max_3/skechers-aero-burst-246210-black-white.jpg" },
+    { label: "Outsole", url: "https://www.skechers.com.my/cdn/shop/files/246210_BKW_C_ee45fdb9-aa06-4aa8-8f3a-f6e17cf553ee.jpg?v=1755793034&width=1445" },
   ],
   "ortuseight-hyperblast-2-1": [
     { label: "Side", url: "https://sportaways.com/storage/products/7814/sepatu-running-ortuseight-hyperblast-21-sacramentolime-s4rv-1.webp" },
@@ -53,20 +86,5 @@ export const galleryFixes: Record<string, ShoeImageView[]> = {
   "specs-cloudblazer": [
     { label: "Side", url: "https://sportaways.com/storage/products/5640/sepatu-running-specs-cloudblazer-green-geckotofu-gv2x-1.webp" },
     { label: "Top", url: "https://sportaways.com/storage/products/5640/sepatu-running-specs-cloudblazer-green-geckotofu-gv2x-4.webp" },
-  ],
-  "on-cloudflow-5": [
-    { label: "Side", url: "https://www.holabirdsports.com/cdn/shop/files/042528_3.jpg?v=1749157957&width=2048" },
-    { label: "Top", url: "https://bananarun.com/media/catalog/product/cache/aa5b17117e43c0b36f21fad92985fb3e/3/m/3mf10113306-cloudflow_5-fw25-arctic_stone-m-1x1-g2.png" },
-    { label: "Outsole", url: "https://images.prodirectsport.com/productimages/Gallery_3/1021860_Gallery_3_2016845.jpg" },
-  ],
-  "skechers-aero-razor": [
-    { label: "Side", url: "https://www.skechers.com.au/media/catalog/product/2/4/246240_wbo_02.jpg?auto=webp&fit=cover&format=pjpg&quality=85&width=1200" },
-    { label: "Top", url: "https://www.sportsdirect.com/images/imgzoom/12/12124501_xxl_a3.jpg" },
-    { label: "Outsole", url: "https://cdn.shopify.com/s/files/1/0603/3031/1875/files/246240_WBO_OUTSOLE_converted_1080x.jpg?=75&v=1771836404" },
-  ],
-  "skechers-aero-burst": [
-    { label: "Side", url: "https://cdn.idealo.com/folder/Product/207854/0/207854090/s2_produktbild_max_2/skechers-aero-burst-246210-black-white.jpg" },
-    { label: "Top", url: "https://cdn.idealo.com/folder/Product/207854/0/207854090/s11_produktbild_max_3/skechers-aero-burst-246210-black-white.jpg" },
-    { label: "Outsole", url: "https://www.skechers.com.my/cdn/shop/files/246210_BKW_C_ee45fdb9-aa06-4aa8-8f3a-f6e17cf553ee.jpg?v=1755793034&width=1445" },
   ],
 };
